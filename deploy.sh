4e9ec5066e1447ca05330859d97083911ac1bfc7
Leo Orpilla III <leoorpilla3@gmail.com>
2020-08-03 19:23:12 +1000

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+DEPLOY="docs/"
+BUILD="_site/"
+
+build() {
+    COMMIT=$(git rev-parse HEAD)
+
+    echo "Building css..."
+    npm run build:css
+
+    echo "Building site..."
+    cabal clean
+    cabal build
+    cabal exec site build
+
+    rm -rf $DEPLOY
+    mkdir $DEPLOY
+
+    cp -r "$BUILD"/* $DEPLOY
+
+    echo "Static site at $DEPLOY"
+
+    git add $DEPLOY
+
+    git commit -m "Add generated site from $COMMIT"
+
+    git push origin develop
+}
+
+build
