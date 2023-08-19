231d9f44890e702432714d2223862964a5d7e1f7
Leo Orpilla III <hi@ldgrp.me>
2023-08-19 14:39:58 +1000

    Fix external post ordering

--- site.hs
+++ site.hs
@@ -1,5 +1,6 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
+import Data.Maybe (fromMaybe)
 import Data.Monoid (mappend)
 import Hakyll
 import System.FilePath.Posix  ((</>))
@@ -7,10 +8,6 @@ import System.FilePath.Posix  ((</>))
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
-    match "fonts/*" $ do
-        route   idRoute
-        compile copyFileCompiler
-
     match "images/*" $ do
         route   idRoute
         compile copyFileCompiler
@@ -35,6 +32,10 @@ main = hakyll $ do
                 >>= loadAndApplyTemplate "templates/post.html"    ideaCtx
                 >>= loadAndApplyTemplate "templates/default.html" ideaCtx
                 >>= relativizeUrls
+    
+    match "posts/external/*.md" $ do
+        route   $ metadataRoute urlRoute
+        compile pandocCompiler
 
     match "posts/*.md" $ do
         route removeDateRoute
@@ -74,7 +75,6 @@ main = hakyll $ do
 
     match "templates/*" $ compile templateBodyCompiler
 
-
 --------------------------------------------------------------------------------
 entryCtx :: String -> Context String
 entryCtx entryType =
@@ -91,27 +91,39 @@ ideaCtx = entryCtx "idea"
 recipeCtx :: Context String
 recipeCtx = entryCtx "recipe"
 
+--------------------------------------------------------------------------------
+posts = do
+    x <- loadAll "posts/*.md"
+    y <- loadAll "posts/external/*.md"
+    pure (x ++ y)
+
+ideas = loadAll "ideas/*.md"
+    
+--------------------------------------------------------------------------------
 archiveCtx :: Context String
 archiveCtx =
-    listField "posts" postCtx (recentFirst =<< loadAll "posts/*.md") `mappend`
-    listField "ideas" ideaCtx (recentFirst =<< loadAll "ideas/*.md") `mappend`
-    constField "title" "Archive"                                     `mappend`
+    listField "posts" postCtx (recentFirst =<< posts) `mappend`
+    listField "ideas" ideaCtx (recentFirst =<< ideas) `mappend`
+    constField "title" "Archive" `mappend`
     defaultContext
     
 indexCtx :: Context String
 indexCtx =
-    listField "posts" postCtx (recentFirst =<< loadAll "posts/*.md") `mappend`
-    listField "ideas" ideaCtx (recentFirst =<< loadAll "ideas/*.md") `mappend`
+    listField "posts" postCtx (recentFirst =<< posts) `mappend`
+    listField "ideas" ideaCtx (recentFirst =<< ideas) `mappend`
     constField "title" "Leo Orpilla III" `mappend`
     defaultContext
 
 ideasCtx :: Context String
 ideasCtx =
-    listField "ideas" ideaCtx (recentFirst =<< loadAll "ideas/*.md") `mappend`
+    listField "ideas" ideaCtx (recentFirst =<< ideas) `mappend`
     constField "title" "Ideas" `mappend`
     defaultContext
 
-
+--------------------------------------------------------------------------------
 removeDateRoute :: Routes
 removeDateRoute = 
   composeRoutes (gsubRoute "[0-9]{4}-[0-9]{2}-[0-9]{2}-" $ const "") (setExtension "html")
+
+urlRoute :: Metadata -> Routes
+urlRoute = constRoute . fromMaybe "\\#" . lookupString "url"
