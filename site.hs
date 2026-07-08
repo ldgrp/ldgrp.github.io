1aafd9c7b02d1e49b77574965029402c864a3f69
Leo Orpilla III <leo@usehall.com>
2026-07-08 22:16:50 +1000

    Add sitemap, robots, atom feed

--- site.hs
+++ site.hs
@@ -1,5 +1,6 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
+import Data.List (isSuffixOf)
 import Data.Maybe (fromMaybe)
 import Data.Monoid (mappend)
 import qualified Data.Text as T
@@ -7,6 +8,18 @@ import Hakyll
 import System.FilePath.Posix  ((</>))
 import System.Process (proc, readCreateProcess)
 
+--------------------------------------------------------------------------------
+root :: String
+root = "https://ldgrp.me"
+
+feedConfiguration :: FeedConfiguration
+feedConfiguration = FeedConfiguration
+    { feedTitle       = "Leo Orpilla III"
+    , feedDescription = "Posts by Leo Orpilla III"
+    , feedAuthorName  = "Leo Orpilla III"
+    , feedAuthorEmail = "leo@ldgrp.me"
+    , feedRoot        = root
+    }
 
 --------------------------------------------------------------------------------
 main :: IO ()
@@ -30,7 +43,7 @@ main = hakyll $ do
     match "404.html" $ do
         route   $ setExtension "html"
         compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= loadAndApplyTemplate "templates/default.html" siteCtx
             >>= relativizeUrls
     
     match "ideas/external/*.md" $ do
@@ -93,6 +106,36 @@ main = hakyll $ do
                 >>= loadAndApplyTemplate "templates/default.html" indexCtx
                 >>= relativizeUrls
 
+    create ["sitemap.xml"] $ do
+        route idRoute
+        compile $ do
+            ps <- loadAll "posts/*.md"
+            ts <- loadAll "posts/*.typ"
+            is <- loadAll "ideas/*.md"
+            rs <- loadAll "recipes/*"
+            sp <- loadAll (fromList ["archive.html", "ideas.html"])
+            let pages = ps ++ ts ++ is ++ rs ++ sp
+                sitemapCtx =
+                    constField "root" root `mappend`
+                    listField "entries" siteCtx (return pages) `mappend`
+                    defaultContext
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/sitemap.xml" sitemapCtx
+
+    create ["atom.xml"] $ do
+        route idRoute
+        compile $ do
+            let feedCtx = bodyField "description" `mappend` postCtx
+            feedPosts <- fmap (take 20) . recentFirst =<< do
+                a <- loadAllSnapshots "posts/*.md" "content"
+                b <- loadAllSnapshots "posts/*.typ" "content"
+                pure (a ++ b)
+            renderAtom feedConfiguration feedCtx feedPosts
+
+    match "robots.txt" $ do
+        route idRoute
+        compile copyFileCompiler
+
     match "templates/*" $ compile templateBodyCompiler
 
 --------------------------------------------------------------------------------
@@ -125,11 +168,29 @@ extractBody = T.unpack . inner . T.pack
       let afterOpen = T.drop 1 . snd . T.breakOn ">" . snd $ T.breakOn "<body" t
       in fst $ T.breakOn "</body>" afterOpen
 
+--------------------------------------------------------------------------------
+siteCtx :: Context String
+siteCtx =
+    constField "root" root `mappend`
+    cleanUrlField `mappend`
+    defaultContext
+
+cleanUrlField :: Context a
+cleanUrlField = field "url" $ \item -> do
+    mroute <- getRoute (itemIdentifier item)
+    case mroute of
+        Nothing -> noResult "no route for item"
+        Just r  -> pure (cleanIndex (toUrl r))
+  where
+    cleanIndex u
+        | "/index.html" `isSuffixOf` u = take (length u - 10) u
+        | otherwise = u
+
 entryCtx :: String -> Context String
 entryCtx entryType =
     constField "entry-type" entryType `mappend`
     dateField "date" "%d %b %Y" `mappend`
-    defaultContext
+    siteCtx
 
 postCtx :: Context String
 postCtx = entryCtx "post"
@@ -159,20 +220,20 @@ archiveCtx =
     listField "posts" postCtx (recentFirst =<< posts) `mappend`
     listField "ideas" ideaCtx (recentFirst =<< ideas) `mappend`
     constField "title" "Archive" `mappend`
-    defaultContext
+    siteCtx
     
 indexCtx :: Context String
 indexCtx =
     listField "posts" postCtx (recentFirst =<< posts) `mappend`
     listField "ideas" ideaCtx (recentFirst =<< ideas) `mappend`
     constField "title" "Leo Orpilla III" `mappend`
-    defaultContext
+    siteCtx
 
 ideasCtx :: Context String
 ideasCtx =
     listField "ideas" ideaCtx (recentFirst =<< ideas) `mappend`
     constField "title" "Ideas" `mappend`
-    defaultContext
+    siteCtx
 
 --------------------------------------------------------------------------------
 removeDateRoute :: Routes
